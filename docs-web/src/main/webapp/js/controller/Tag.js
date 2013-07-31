fc5a1f2e7126a63f7db8b554e264728b631a6672
jendib <benjamin.gam@gmail.com>
2013-07-31 22:07:42 +0200

    Confirmation on tag deletion

--- docs-web/src/main/webapp/js/controller/Tag.js
+++ docs-web/src/main/webapp/js/controller/Tag.js
@@ -3,7 +3,7 @@
 /**
  * Tag controller.
  */
-App.controller('Tag', function($scope, $state, Tag, Restangular) {
+App.controller('Tag', function($scope, $dialog, $state, Tag, Restangular) {
   // Retrieve tags
   Tag.tags().then(function(data) {
     $scope.tags = data.tags;
@@ -24,10 +24,20 @@ App.controller('Tag', function($scope, $state, Tag, Restangular) {
    * Delete a tag.
    */
   $scope.deleteTag = function(tag) {
-    Restangular.one('tag', tag.id).remove().then(function() {
-      $scope.tags = _.reject($scope.tags, function(t) {
-        return tag.id == t.id;
-      });
+    var title = 'Delete tag';
+    var msg = 'Do you really want to delete this tag?';
+    var btns = [{result:'cancel', label: 'Cancel'}, {result:'ok', label: 'OK', cssClass: 'btn-primary'}];
+
+    $dialog.messageBox(title, msg, btns)
+    .open()
+    .then(function(result) {
+      if (result == 'ok') {
+        Restangular.one('tag', tag.id).remove().then(function() {
+          $scope.tags = _.reject($scope.tags, function(t) {
+            return tag.id == t.id;
+          });
+        });
+      }
     });
   };
   
