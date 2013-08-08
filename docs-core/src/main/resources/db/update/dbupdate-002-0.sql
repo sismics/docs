f328aedccd596aad9fe2c27eceb33fdf6c70f70b
jendib <benjamin.gam@gmail.com>
2013-08-08 10:46:15 +0200

    Add default value for new column

--- docs-core/src/main/resources/db/update/dbupdate-002-0.sql
+++ docs-core/src/main/resources/db/update/dbupdate-002-0.sql
@@ -1,3 +1,2 @@
-alter table T_TAG add column TAG_COLOR_C varchar(7) not null;
-update T_TAG set TAG_COLOR_C = '#3a87ad';
+alter table T_TAG add column TAG_COLOR_C varchar(7) default '#3a87ad' not null;
 update T_CONFIG set CFG_VALUE_C='2' where CFG_ID_C='DB_VERSION';
