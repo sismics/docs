c37fb1d4aecee352b09b1c7215554d102203adac
jendib <benjamin.gam@gmail.com>
2013-08-12 21:20:34 +0200

    Hide admin only menu in settings tab

--- docs-web/src/main/webapp/js/controller/Settings.js
+++ docs-web/src/main/webapp/js/controller/Settings.js
@@ -4,4 +4,8 @@
  * Settings controller.
  */
 App.controller('Settings', function($scope, Restangular) {
+  // Flag if the user is admin
+  $scope.userInfo.then(function (data) {
+    $scope.isAdmin = data.base_functions.indexOf('ADMIN') != -1;
+  });
 });
