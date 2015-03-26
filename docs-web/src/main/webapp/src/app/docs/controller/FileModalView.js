3461804399db15bcec8c7a74fa2247d95ac76874
jendib <benjamin.gam@gmail.com>
2015-03-27 00:50:00 +0100

    Drag & drop to upload orphan files

--- docs-web/src/main/webapp/src/app/docs/controller/FileModalView.js
+++ docs-web/src/main/webapp/src/app/docs/controller/FileModalView.js
@@ -4,6 +4,8 @@
  * File modal view controller.
  */
 angular.module('docs').controller('FileModalView', function($rootScope, $modalInstance, $scope, $state, $stateParams, Restangular) {
+  var view = $stateParams.id ? 'document.view.file' : 'document.default.file';
+
   // Load files
   Restangular.one('file').getList('list', { id: $stateParams.id }).then(function(data) {
     $scope.files = data.files;
@@ -24,7 +26,7 @@ angular.module('docs').controller('FileModalView', function($rootScope, $modalIn
       if (value.id == $stateParams.fileId) {
         var next = $scope.files[key + 1];
         if (next) {
-          $state.transitionTo('document.view.file', { id: $stateParams.id, fileId: next.id });
+          $state.transitionTo(view, { id: $stateParams.id, fileId: next.id });
         }
       }
     });
@@ -38,7 +40,7 @@ angular.module('docs').controller('FileModalView', function($rootScope, $modalIn
       if (value.id == $stateParams.fileId) {
         var previous = $scope.files[key - 1];
         if (previous) {
-          $state.transitionTo('document.view.file', { id: $stateParams.id, fileId: previous.id });
+          $state.transitionTo(view, { id: $stateParams.id, fileId: previous.id });
         }
       }
     });
@@ -72,7 +74,7 @@ angular.module('docs').controller('FileModalView', function($rootScope, $modalIn
   // Close the modal when the user exits this state
   var off = $rootScope.$on('$stateChangeStart', function(event, toState) {
     if (!$modalInstance.closed) {
-      if (toState.name == 'document.view.file') {
+      if (toState.name == view) {
         $modalInstance.close();
       } else {
         $modalInstance.dismiss();
