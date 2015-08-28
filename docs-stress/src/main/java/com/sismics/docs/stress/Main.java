374310d13c727afa972928d663c4de3070f7c453
jendib <benjamin.gam@gmail.com>
2015-08-29 00:12:15 +0200

    Init stress app

---
+++ docs-stress/src/main/java/com/sismics/docs/stress/Main.java
@@ -0,0 +1,25 @@
+package com.sismics.docs.stress;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Stress app for Sismics Docs.
+ * 
+ * @author bgamard
+ */
+public class Main {
+    /**
+     * Logger.
+     */
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    
+    /**
+     * Entry point.
+     * 
+     * @param args Args
+     */
+    public static void main(String[] args) {
+        log.info("Starting stress test...");
+    }
+}
