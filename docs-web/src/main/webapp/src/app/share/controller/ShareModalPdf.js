83e1191a8a84203357671e06d5e5c4144d43b312
jendib <benjamin.gam@gmail.com>
2016-01-01 21:38:25 +0100

    #55: Export document in PDF (Share UI)

---
+++ docs-web/src/main/webapp/src/app/share/controller/ShareModalPdf.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * Document modal PDF controller.
+ */
+angular.module('share').controller('ShareModalPdf', function ($scope, $window, $stateParams, $modalInstance) {
+  $scope.export = {
+    metadata: false,
+    comments: false,
+    fitimagetopage: true,
+    margin: 10
+  };
+
+  // Export to PDF
+  $scope.exportPdf = function() {
+    $window.open('../api/document/' + $stateParams.documentId
+        + '/pdf?metadata=' + $scope.export.metadata
+        + '&comments=' + $scope.export.comments
+        + '&fitimagetopage=' + $scope.export.fitimagetopage
+        + '&margin=' + $scope.export.margin
+        + '&share=' + $stateParams.shareId);
+
+    $modalInstance.close();
+  };
+
+  // Close the modal
+  $scope.close = function () {
+    $modalInstance.close();
+  }
+});
