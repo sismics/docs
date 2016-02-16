adebb7ff6d6dd84529b5fe0d47b2c9bc10e4e8aa
jendib <benjamin.gam@gmail.com>
2016-02-17 00:28:48 +0100

    #70: User profiles UI

--- docs-web/src/main/webapp/src/app/docs/controller/User.js
+++ docs-web/src/main/webapp/src/app/docs/controller/User.js
@@ -3,5 +3,14 @@
 /**
  * User controller.
  */
-angular.module('docs').controller('User', function() {
+angular.module('docs').controller('User', function(Restangular, $scope, $state) {
+  // Load users
+  Restangular.one('user/list').get({ limit: 100 }).then(function(data) {
+    $scope.users = data.users;
+  });
+
+  // Open a user
+  $scope.openUser = function(user) {
+    $state.go('user.profile', { username: user.username });
+  };
 });
