7d7adeeca0ca6e093952c45a24f0f0110f619d24
jendib <benjamin.gam@gmail.com>
2016-04-13 01:29:03 +0200

    #79: CSS generator

--- docs-web/src/main/java/com/sismics/docs/rest/resource/ThemeResource.java
+++ docs-web/src/main/java/com/sismics/docs/rest/resource/ThemeResource.java
@@ -5,6 +5,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import com.sismics.util.css.Selector;
+
 /**
  * Theme REST resources.
  * 
@@ -22,8 +24,7 @@ public class ThemeResource extends BaseResource {
     @Produces("text/css")
     public Response stylesheet() {
     	StringBuilder sb = new StringBuilder();
-    	sb.append("body {\n");
-    	sb.append("}");
+    	sb.append(new Selector("body"));
         return Response.ok().entity(sb.toString()).build();
     }
 }
