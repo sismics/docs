79141edf7044eaddf33d1251543b263aebd67d9b
jendib <benjamin.gam@gmail.com>
2016-05-09 22:09:29 +0200

    Closes #97: Handle write permission in #/tag and #/tag/id

--- docs-web/src/main/webapp/src/app/docs/controller/tag/Tag.js
+++ docs-web/src/main/webapp/src/app/docs/controller/tag/Tag.js
@@ -3,13 +3,16 @@
 /**
  * Tag controller.
  */
-angular.module('docs').controller('Tag', function($scope, $dialog, Restangular, $state) {
+angular.module('docs').controller('Tag', function($scope, Restangular, $state) {
   $scope.tag = { name: '', color: '#3a87ad' };
 
   // Retrieve tags
-  Restangular.one('tag/list').get().then(function(data) {
-    $scope.tags = data.tags;
-  });
+  $scope.loadTags = function() {
+    Restangular.one('tag/list').get().then(function(data) {
+      $scope.tags = data.tags;
+    });
+  };
+  $scope.loadTags();
 
   /**
    * Display a tag.
@@ -25,28 +28,6 @@ angular.module('docs').controller('Tag', function($scope, $dialog, Restangular, 
     Restangular.one('tag').put($scope.tag).then(function(data) {
       $scope.tags.push({ id: data.id, name: $scope.tag.name, color: $scope.tag.color });
       $scope.tag = { name: '', color: '#3a87ad' };
-    });
-  };
-  
-  /**
-   * Delete a tag.
-   */
-  $scope.deleteTag = function(tag) {
-    var title = 'Delete tag';
-    var msg = 'Do you really want to delete this tag?';
-    var btns = [
-      {result: 'cancel', label: 'Cancel'},
-      {result: 'ok', label: 'OK', cssClass: 'btn-primary'}
-    ];
-
-    $dialog.messageBox(title, msg, btns, function(result) {
-      if (result == 'ok') {
-        Restangular.one('tag', tag.id).remove().then(function() {
-          $scope.tags = _.reject($scope.tags, function(t) {
-            return tag.id == t.id;
-          });
-        });
-      }
     });
   };
 });
