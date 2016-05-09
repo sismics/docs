b1e58396d19e4764ca74ea4429dc425fd479b0ce
jendib <benjamin.gam@gmail.com>
2016-05-09 21:53:15 +0200

    Closes #98: Fix inherited permissions table

--- docs-web/src/main/webapp/src/app/docs/controller/document/DocumentViewPermissions.js
+++ docs-web/src/main/webapp/src/app/docs/controller/document/DocumentViewPermissions.js
@@ -7,7 +7,7 @@ angular.module('docs').controller('DocumentViewPermissions', function($scope) {
   // Watch for ACLs change and group them for easy displaying
   $scope.$watch('document.inherited_acls', function(acls) {
     $scope.inheritedAcls = _.groupBy(acls, function(acl) {
-      return acl.source_id;
+      return acl.source_id + acl.id;
     });
   });
 });
