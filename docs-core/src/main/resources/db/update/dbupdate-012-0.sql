f46e10e11ccbb18e37b76e42e6d4b9de9d57cdd6
Benjamin Gamard <b.gamard@sismics.com>
2017-10-31 21:20:22 +0100

    support more languages

--- docs-core/src/main/resources/db/update/dbupdate-012-0.sql
+++ docs-core/src/main/resources/db/update/dbupdate-012-0.sql
@@ -1,2 +1,2 @@
-alter table T_DOCUMENT alter column DOC_LANGUAGE_C set varchar(7) default 'eng' not null;
+alter table T_DOCUMENT alter column DOC_LANGUAGE_C varchar(7) default 'eng' not null;
 update T_CONFIG set CFG_VALUE_C = '12' where CFG_ID_C = 'DB_VERSION';
