d2256eabfa8a666070a0a9580b640182b26174f9
Benjamin Gamard <b.gamard@sismics.com>
2018-03-23 09:53:18 +0100

    Closes #216: redirect to login if not connected

--- docs-web/src/main/webapp/src/app/docs/controller/Main.js
+++ docs-web/src/main/webapp/src/app/docs/controller/Main.js
@@ -5,7 +5,11 @@
  */
 angular.module('docs').controller('Main', function($scope, $rootScope, $state, User) {
   User.userInfo().then(function(data) {
-    if (!data.anonymous) {
+    if (data.anonymous) {
+      $state.go('login', {}, {
+        location: 'replace'
+      });
+    } else {
       $state.go('document.default', {}, {
         location: 'replace'
       });
