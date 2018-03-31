90a3d7aa68b56eaf439e056049834d0d016bcf81
Benjamin Gamard <b.gamard@sismics.com>
2018-03-31 22:53:49 +0200

    add missing index

--- docs-core/src/main/java/com/sismics/util/EnvironmentUtil.java
+++ docs-core/src/main/java/com/sismics/util/EnvironmentUtil.java
@@ -3,28 +3,28 @@ package com.sismics.util;
 /**
  * Environment properties utilities.
  *
- * @author jtremeaux 
+ * @author jtremeaux
  */
 public class EnvironmentUtil {
 
     private static String OS = System.getProperty("os.name").toLowerCase();
-    
+
     private static String APPLICATION_MODE = System.getProperty("application.mode");
 
     private static String WINDOWS_APPDATA = System.getenv("APPDATA");
 
     private static String MAC_OS_USER_HOME = System.getProperty("user.home");
-    
+
     private static String DOCS_HOME = System.getProperty("docs.home");
 
     /**
      * In a web application context.
      */
     private static boolean webappContext;
-    
+
     /**
      * Returns true if running under Microsoft Windows.
-     * 
+     *
      * @return Running under Microsoft Windows
      */
     public static boolean isWindows() {
@@ -33,7 +33,7 @@ public class EnvironmentUtil {
 
     /**
      * Returns true if running under Mac OS.
-     * 
+     *
      * @return Running under Mac OS
      */
     public static boolean isMacOs() {
@@ -42,20 +42,20 @@ public class EnvironmentUtil {
 
     /**
      * Returns true if running under UNIX.
-     * 
+     *
      * @return Running under UNIX
      */
     public static boolean isUnix() {
         return OS.contains("nix") || OS.contains("nux") || OS.contains("aix");
     }
-    
+
     /**
      * Returns true if we are in a unit testing environment.
-     * 
+     *
      * @return Unit testing environment
      */
     public static boolean isUnitTest() {
-        return !webappContext || isDevMode();
+        return !webappContext;
     }
 
     /**
@@ -66,10 +66,10 @@ public class EnvironmentUtil {
     public static boolean isDevMode() {
         return "dev".equalsIgnoreCase(APPLICATION_MODE);
     }
-    
+
     /**
      * Returns the MS Windows AppData directory of this user.
-     * 
+     *
      * @return AppData directory
      */
     public static String getWindowsAppData() {
@@ -78,7 +78,7 @@ public class EnvironmentUtil {
 
     /**
      * Returns the Mac OS home directory of this user.
-     * 
+     *
      * @return Home directory
      */
     public static String getMacOsUserHome() {
@@ -87,7 +87,7 @@ public class EnvironmentUtil {
 
     /**
      * Returns the home directory of DOCS (e.g. /var/docs).
-     * 
+     *
      * @return Home directory
      */
     public static String getDocsHome() {
